9d8b2becad7227808231aeea7e86cf9b2beb3e6e
FreeToGo <freetogo@gmail.com>
2019-10-15 18:08:12 +0800

    ...

---
+++ noip-renew.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+USERNAME=""
+PASSWORD=""
+NUM_HOSTS=3 # make sure to change this to the number of configured dynamic hosts on your no-ip account
+
+LOGDIR=$1
+PROGDIR=$(dirname $0)
+
+if [ -z "$LOGDIR" ]; then
+    $PROGDIR/noip-renew.py "$USERNAME" "$PASSWORD" $NUM_HOSTS 2
+else
+    cd $LOGDIR
+    $PROGDIR/noip-renew.py "$USERNAME" "$PASSWORD" $NUM_HOSTS 0 >> $USERNAME.log
+fi
+
